0b6c744c977f2c16901a05bf7736e6f0eddb60eb
noble <1234@gmail.com>
2024-02-23 18:24:23 +0900

    検索結果ページのUIの完成

--- lib/pages/SearchResult.dart
+++ lib/pages/SearchResult.dart
@@ -1,10 +1,10 @@
-import 'dart:html';
-import 'dart:math';
+// import 'dart:html';
+// import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final colorProvider = StateProvider<bool>((ref) => false);
 
 class SearchResult extends ConsumerWidget {
   const SearchResult({super.key});
@@ -15,24 +15,20 @@ class SearchResult extends ConsumerWidget {
         appBar: AppBar(
           backgroundColor: Colors.purple[100],
           centerTitle: true,
-          leading: const Icon(
-            Icons.clear_rounded,
-            size: 30,
-          ),
           title: const Text(
             "検索結果の一覧",
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(right: 16.0),
-              child: Icon(
-                Icons.star,
-                size: 30,
-                color: Colors.white,
-              ),
-            )
-          ],
+          // actions: const [
+          //   Padding(
+          //     padding: EdgeInsets.only(right: 16.0),
+          //     child: Icon(
+          //       Icons.star,
+          //       size: 30,
+          //       color: Colors.white,
+          //     ),
+          //   )
+          // ],
         ),
         body: SingleChildScrollView(
           child: Column(
@@ -179,104 +175,201 @@ class SearchResult extends ConsumerWidget {
                   ),
                 ],
               ),
-              LimitedBox(
-                maxHeight: 540,
-                child: ListView(
-                  shrinkWrap: true,
-                  children: <Widget>[
-                    ListTile(
-                      leading: Icon(Icons.map),
-                      title: Text('Map'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.photo_album),
-                      title: Text('Album'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text('Phone'),
-                    ),
-                  ],
+              Padding(
+                padding:
+                    const EdgeInsets.only(right: 8.0, left: 8.0, top: 16.0),
+                child: LimitedBox(
+                  maxHeight: 540,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      busSearchResult(77, 500, 30, 3, '14:30', '15:30',
+                          '沖縄高専入口', '那覇バスターミナル'),
+                      busSearchResult(77, 500, 30, 3, '14:30', '15:30',
+                          '沖縄高専入口', '那覇バスターミナル'),
+                      busSearchResult(77, 500, 30, 3, '14:30', '15:30',
+                          '沖縄高専入口', '那覇バスターミナル'),
+                      busSearchResult(77, 500, 30, 3, '14:30', '15:30',
+                          '沖縄高専入口', '那覇バスターミナル'),
+                    ],
+                  ),
                 ),
               ),
             ],
           ),
         ));
+  }
+
+  Widget busSearchResult(
+    int busNum, //バスの番号：77番
+    int cost, //バスの値段：500円
+    int time, //バスの所要時間：30分
+    int delayTime, //バスの遅延時間：3分遅れ
+    String startTime, //バスの発車時刻,14:30
+    String endTime, //バスの到着時刻,15:30
+    String startBusStop, //乗るバス停の名前：沖縄高専入口
+    String endBusStop, //降りるバス停の名前：那覇バスターミナル
+  ) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.black38,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                      ),
+                      child: Text(
+                        busNum.toString(),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                  child: Text(
+                    startBusStop,
+                    style: const TextStyle(
+                        fontSize: 17, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                BusRegisterationButton(provider: colorProvider),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "$cost円　$time分",
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                color: Colors.white,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "約$delayTime分遅れ",
+                      style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        startTime,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        startBusStop,
+                        style: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      const Text(
+                        "(那覇行き)",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Icon(
+                  Icons.arrow_forward_rounded,
+                  size: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        endTime,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        endBusStop,
+                        style: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      const Text(
+                        "(那覇行き)",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BusRegisterationButton extends ConsumerWidget {
+  const BusRegisterationButton({super.key, required this.provider});
+
+  final StateProvider<bool> provider;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final color = ref.watch(provider);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          ref.read(provider.notifier).state = !color;
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.directions_bus_filled_sharp,
+                color: color ? Colors.white : Colors.black)
+          ],
+        ),
+      ),
+    );
   }
 }
