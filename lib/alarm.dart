b44d3fc87bc89a1743c1e62a1843c0fe590afdc9
SeinKina <ajklfdjakl@gmail.com>
2024-02-25 22:47:01 +0900

    アラームなったらダイアログ表示する

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -1,6 +1,6 @@
-import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
 List<String> alarms = [
   'Clock-Alarm05-1(Mid)',
@@ -21,126 +21,46 @@ class AlarmTest extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: Text(alarms[2]),
-          onPressed: () async {
-            await Alarm.init();
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('テスト');
+                FlutterRingtonePlayer.play(
+                  fromAsset: 'assets/alarms/${alarms[2]}.mp3',
+                  asAlarm: true,
+                  looping: false,
+                );
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text("タイトル"),
+                      content: Text("メッセージ内容"),
+                      actions: [
+                        TextButton(
+                          child: Text("OK"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text(alarms[2]),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FlutterRingtonePlayer.stop();
+              },
+              child: Text('ストップ'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-class Alarms extends ConsumerWidget {
-  const Alarms({super.key, required this.alarmPath});
-
-  final String alarmPath;
-
-  Future<void> alarmSet() async {
-    await Alarm.init();
-
-    // アラームの設定を作成
-    final alarmSettings = AlarmSettings(
-      id: 42,
-      dateTime: DateTime.now().add(const Duration(seconds: 3)),
-      assetAudioPath: 'assets/alarms/$alarmPath.mp3',
-      loopAudio: false,
-      vibrate: true,
-      fadeDuration: 3.0,
-      notificationTitle: 'This is the title',
-      notificationBody: 'This is the body',
-      enableNotificationOnKill: true,
-    );
-
-    // アラームを登録
-    await Alarm.set(alarmSettings: alarmSettings);
-  }
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return ElevatedButton(
-      child: Text('$alarmPath'),
-      onPressed: () {
-        alarmSet();
-      },
-    );
-  }
-}
-
-// import 'package:alarm/alarm.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// List<String> alarms = [
-//   'Clock-Alarm05-1(Mid)',
-//   'Clock-Alarm05-2(Low)',
-//   'Clock-Alarm05-3(Mid-Loop)',
-//   'Clock-Alarm05-4(Low-Loop)',
-//   'Clock-Alarm05-5(Toggle)',
-//   'Clock-Alarm05-6(Far-Mid)',
-//   'Clock-Alarm05-7(Far-Low)',
-//   'Clock-Alarm05-8(Far-Mid-Loop)',
-//   'Clock-Alarm05-9(Far-Low-Loop)',
-// ];
-
-// class AlarmTest extends ConsumerWidget {
-//   const AlarmTest({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final alarm = Alarms(alarmPath: alarms[2]);
-
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             ElevatedButton(
-//               child: Text(alarms[2]),
-//               onPressed: () {
-//                 alarm.alarmSet();
-//               },
-//             ),
-//             ElevatedButton(
-//               child: Text('ストップ'),
-//               onPressed: () {
-//                 alarm.alarmStop();
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class Alarms {
-//   const Alarms({required this.alarmPath});
-
-//   final String alarmPath;
-
-//   Future<void> alarmSet() async {
-//     await Alarm.init();
-
-//     // アラームの設定を作成
-//     final alarmSettings = AlarmSettings(
-//       id: 42,
-//       dateTime: DateTime.now().add(const Duration(seconds: 3)),
-//       assetAudioPath: 'assets/alarms/$alarmPath.mp3',
-//       loopAudio: true,
-//       vibrate: true,
-//       fadeDuration: 3.0,
-//       notificationTitle: 'This is the title',
-//       notificationBody: 'This is the body',
-//       enableNotificationOnKill: true,
-//     );
-
-//     // アラームを登録
-//     await Alarm.set(alarmSettings: alarmSettings);
-//   }
-
-//   Future<void> alarmStop() async {
-//     await Alarm.stop(42);
-//   }
-// }
