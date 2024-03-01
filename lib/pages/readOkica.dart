8d3a576156337e8e67eb1e7a581b7d46cfbe0b82
noble <1234@gmail.com>
2024-03-02 01:06:16 +0900

    テスト２！！！！

--- lib/pages/readOkica.dart
+++ lib/pages/readOkica.dart
@@ -1,60 +1,207 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import '../backgrounds/extensions.dart';
-import '../backgrounds/form_row.dart';
-import '../backgrounds/nfc_session.dart';
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 import 'package:nfc_manager/platform_tags.dart';
-import 'package:provider/provider.dart';
 
-class TagReadModel with ChangeNotifier {
-  NfcTag? tag;
+void main() {
+  runApp(const ReadOkica());
+}
 
-  Map<String, dynamic>? additionalData;
+class ReadOkica extends StatelessWidget {
+  const ReadOkica({Key? key}) : super(key: key);
 
-  Future<String?> handleTag(NfcTag tag) async {
-    this.tag = tag;
-    additionalData = {};
-    Object? tech;
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: NfcReadPage(),
+    );
+  }
+}
 
-    // todo: more additional data
-    if (Platform.isIOS) {
-      tech = FeliCa.from(tag);
-      final moneyServiceCode = Uint8List.fromList([0x50, 0xD7]);
-      final serviceNotFound = Uint8List.fromList([0xff, 0xff]);
-      final moneyBite = [
-        for (int i = 0; i < 12; i++) Uint8List.fromList([0x80, i]),
-      ];
+class NfcReadPage extends StatefulWidget {
+  const NfcReadPage({Key? key}) : super(key: key);
 
-      if (tech is FeliCa) {
-        final polling = await tech.polling(
-          systemCode: tech.currentSystemCode,
-          requestCode: FeliCaPollingRequestCode.noRequest,
-          timeSlot: FeliCaPollingTimeSlot.max1,
-        );
+  @override
+  State<NfcReadPage> createState() => _NfcReadPageState();
+}
 
-        final requestServiceRes =
-            await tech.requestService(nodeCodeList: [moneyServiceCode]);
+class _NfcReadPageState extends State<NfcReadPage> {
+  int balance = 0;
 
-        if (requestServiceRes == serviceNotFound) {
-          additionalData!['Success'] = false;
-        } else {
-          additionalData!['Success'] = true;
-        }
-
-        final moneyRes = await tech.readWithoutEncryption(
-            serviceCodeList: [moneyServiceCode], blockList: moneyBite);
-
-        additionalData!['Money'] = moneyRes;
-      }
-    }
-
-    notifyListeners();
-    return '[Tag - Read] is completed.';
+  @override
+  void initState() {
+    super.initState();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: FutureBuilder(
+          future: NfcManager.instance.isAvailable(),
+          builder: (context, ss) {
+            if (ss.data == true) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('$balance 円'),
+                    ElevatedButton(
+                      onPressed: () async {
+                        NfcManager.instance.startSession(
+                          onDiscovered: (tag) async {
+                            final systemCode = [0x8F, 0xC1];
+                            final serviceCode = [0x50, 0xD7];
+                            final pollingRes =
+                                await polling(tag, systemCode: systemCode);
+                            final idm = pollingRes.sublist(2, 10);
+                            final requestServiceRes = await requestService(
+                              tag,
+                              idm: idm,
+                              serviceCode: serviceCode,
+                            );
+                            // 情報が存在すれば残高情報を取得する
+                            if (requestServiceRes[11] == 00 &&
+                                requestServiceRes[12] == 0) {
+                              final readWithoutEncryptionRes =
+                                  await readWithoutEncryption(
+                                tag,
+                                idm: idm,
+                                serviceCode: serviceCode,
+                                blockCount: 1,
+                              );
+                              final balance = parse(readWithoutEncryptionRes);
+                              setState(() {
+                                this.balance = balance;
+                              });
+                            }
+                          },
+                        );
+                      },
+                      child: const Text('読み込む'),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return const Center(
+              child: Text('対応していません'),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  /// Polling
+  Future<Uint8List> polling(
+    NfcTag tag, {
+    required List<int> systemCode,
+  }) async {
+    final List<int> packet = [];
+    if (Platform.isAndroid) {
+      packet.add(0x06);
+    }
+    packet.add(0x00);
+    packet.addAll(systemCode.reversed);
+    packet.add(0x01);
+    packet.add(0x0F);
+
+    final command = Uint8List.fromList(packet);
+
+    late final Uint8List? res;
+
+    if (NfcF.from(tag) != null) {
+      final nfcf = NfcF.from(tag);
+      res = await nfcf?.transceive(data: command);
+    } else if (FeliCa.from(tag) != null) {
+      final felica = FeliCa.from(tag);
+      res = await felica?.sendFeliCaCommand(command);
+    }
+    if (res == null) {
+      throw Exception();
+    }
+    return res;
+  }
+
+  /// RequestService
+  Future<Uint8List> requestService(
+    NfcTag tag, {
+    required Uint8List idm,
+    required List<int> serviceCode,
+  }) async {
+    final nodeCodeList = Uint8List.fromList(serviceCode);
+    final List<int> packet = [];
+    if (Platform.isAndroid) {
+      packet.add(0x06);
+    }
+    packet.add(0x02);
+    packet.addAll(idm);
+    packet.add(nodeCodeList.elementSizeInBytes);
+    packet.addAll(serviceCode.reversed);
+
+    final command = Uint8List.fromList(packet);
+
+    late final Uint8List? res;
+
+    if (NfcF.from(tag) != null) {
+      final nfcf = NfcF.from(tag);
+      res = await nfcf?.transceive(data: command);
+    } else if (FeliCa.from(tag) != null) {
+      final felica = FeliCa.from(tag);
+      res = await felica?.sendFeliCaCommand(command);
+    }
+    if (res == null) {
+      throw Exception();
+    }
+    return res;
+  }
+
+  /// ReadWithoutEncryption
+  Future<Uint8List> readWithoutEncryption(
+    NfcTag tag, {
+    required Uint8List idm,
+    required List<int> serviceCode,
+    required int blockCount,
+  }) async {
+    final List<int> packet = [];
+    if (Platform.isAndroid) {
+      packet.add(0);
+    }
+    packet.add(0x06);
+    packet.addAll(idm);
+    packet.add(serviceCode.length ~/ 2);
+    packet.addAll(serviceCode.reversed);
+    packet.add(blockCount);
+
+    for (int i = 0; i < blockCount; i++) {
+      packet.add(0x80);
+      packet.add(i);
+    }
+    if (Platform.isAndroid) {
+      packet[0] = packet.length;
+    }
+
+    final command = Uint8List.fromList(packet);
+
+    late final Uint8List? res;
+
+    if (NfcF.from(tag) != null) {
+      final nfcf = NfcF.from(tag);
+      res = await nfcf?.transceive(data: command);
+    } else if (FeliCa.from(tag) != null) {
+      final felica = FeliCa.from(tag);
+      res = await felica?.sendFeliCaCommand(command);
+    }
+    if (res == null) {
+      throw Exception();
+    }
+    return res;
+  }
+
+  /// バイトデータの変換
   int parse(Uint8List rweRes) {
     if (rweRes[10] != 0x00) {
       throw Exception();
@@ -73,96 +220,5 @@ class TagReadModel with ChangeNotifier {
     final balanceData =
         Uint8List.fromList(data[0].sublist(0, 4).reversed.toList());
     return balanceData.buffer.asByteData().getInt32(0);
-  }
-}
-
-class TagReadPage extends StatelessWidget {
-  static Widget withDependency() => ChangeNotifierProvider<TagReadModel>(
-        create: (context) => TagReadModel(),
-        child: TagReadPage(),
-      );
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tag - Read'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(2),
-        children: [
-          FormSection(
-            children: [
-              FormRow(
-                title: Text('Start Session',
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary)),
-                onTap: () => startSession(
-                  context: context,
-                  handleTag: Provider.of<TagReadModel>(context, listen: false)
-                      .handleTag,
-                ),
-              ),
-            ],
-          ),
-          // consider: Selector<Tuple<{TAG}, {ADDITIONAL_DATA}>>
-          Consumer<TagReadModel>(builder: (context, model, _) {
-            final tag = model.tag;
-            final additionalData = model.additionalData;
-            if (tag != null && additionalData != null)
-              return _TagInfo(tag, additionalData);
-            return SizedBox.shrink();
-          }),
-        ],
-      ),
-    );
-  }
-}
-
-class _TagInfo extends StatelessWidget {
-  _TagInfo(this.tag, this.additionalData);
-
-  final NfcTag tag;
-
-  final Map<String, dynamic> additionalData;
-
-  @override
-  Widget build(BuildContext context) {
-    final tagWidgets = <Widget>[];
-    final ndefWidgets = <Widget>[];
-
-    Object? tech;
-
-    if (Platform.isIOS) {
-      tech = FeliCa.from(tag);
-      if (tech is FeliCa) {
-        tagWidgets.add(FormRow(
-          title: Text('Credit'),
-          subtitle: Text(additionalData["Money"].toString() + "円"),
-        ));
-        tagWidgets.add(FormRow(
-          title: Text('Current IDm'),
-          subtitle: Text('${tech.currentIDm.toHexString()}'),
-        ));
-        tagWidgets.add(FormRow(
-          title: Text('Current System Code'),
-          subtitle: Text('${tech.currentSystemCode.toHexString()}'),
-        ));
-      }
-    }
-
-    return Column(
-      children: [
-        FormSection(
-          header: Text('TAG'),
-          children: tagWidgets,
-        ),
-        if (ndefWidgets.isNotEmpty)
-          FormSection(
-            header: Text('NDEF'),
-            children: ndefWidgets,
-          ),
-      ],
-    );
   }
 }
